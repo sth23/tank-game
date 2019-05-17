81bc052213732edff80331b8a75dcf64b9417308
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-17 13:13:22 -0400

    Updated from Brython Server: 5/17/2019 1:13:21 PM

--- tank-game.py
+++ tank-game.py
@@ -173,6 +173,10 @@ class TankGame(App):
                 self.turrainheight += 50
             Turrain(RectangleAsset(self.turrainwidth, self.height * 2, noline, black), (x * self.turrainwidth, self.turrainheight))
             print("x: " + str(x))
+            if x == self.player1.x:
+                self.player1.y = self.turrainheight
+            if x == self.player2.x:
+                self.player2.y = self.turrainheight
     
     def step(self):
         self.player1.step()
