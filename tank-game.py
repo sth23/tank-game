92dcf6a324c402fd0e7971f6ccf26c3d94f4834c
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-17 11:48:28 -0400

    Updated from Brython Server: 5/17/2019 11:48:28 AM

--- tank-game.py
+++ tank-game.py
@@ -156,7 +156,7 @@ class TankGame(App):
     def createTurrain(self):
         for x in range(0, self.width // 10):
             self.turrainheight = self.turrainheight + random.randint(-40, 40)
-            Turrain(RectangleAsset(self.turrainwidth, self.height, noline, blue), (x * self.turrainwidth, self.turrainheight))
+            Turrain(RectangleAsset(self.turrainwidth, self.height, noline, black), (x * self.turrainwidth, self.turrainheight))
             print(self.turrainheight)
     
     def step(self):
