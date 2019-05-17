05d16844937a8b5a86d4129c38729bb5f6a2dd50
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-17 12:05:05 -0400

    Updated from Brython Server: 5/17/2019 12:05:04 PM

--- tank-game.py
+++ tank-game.py
@@ -173,7 +173,7 @@ class TankGame(App):
                 if bullet.collidingWithSprites(Turret):
                     Explosion((bullet.x, bullet.y))
                     bullet.destroy()
-                    if self.player1.turn == False:
+                    if self.player1.turn == True:
                         print("Player 1 wins!")
                         self.player1.turn = True
                         self.player2.turn = False
