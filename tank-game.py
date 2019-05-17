9db128ea62eff6aec4e93f2e3857a530716452f3
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-17 13:01:46 -0400

    Updated from Brython Server: 5/17/2019 1:01:45 PM

--- tank-game.py
+++ tank-game.py
@@ -158,7 +158,7 @@ class TankGame(App):
             print("Player 1 turn")
             
     def placePlayers(self):
-        self.player1.x = random.randint(2, (self.width // ) self.turrainwidth// 3)
+        self.player1.x = random.randint(2, (self.width // self.turrainwidth) // 3)
         print(self.player1.x)
         self.player2.x = random.randint((self.width // self.turrainwidth) * 2 // 3, self.width // self.turrainwidth - 2)
         print(self.player2.x)
