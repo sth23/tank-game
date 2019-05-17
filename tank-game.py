852f1a4836a2777894ac4f79ffd17450a001a2a1
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-17 11:47:50 -0400

    Updated from Brython Server: 5/17/2019 11:47:49 AM

--- tank-game.py
+++ tank-game.py
@@ -9,8 +9,18 @@ from ggame import App, RectangleAsset, CircleAsset, LineAsset, ImageAsset, Frame
 import math
 import random
 
-black = Color(0, 1)
+# Colors & lines
+red = Color(0xff0000, 1.0)
+orange = Color(0xffa500, 1.0)
+yellow = Color(0xffff00, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+purple = Color(0x800080, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+gray = Color(0x888888, 0.5)
 noline = LineStyle(0, black)
+whiteline = LineStyle(1, white)
 
 class Explosion(Sprite):
     asset = ImageAsset("explosion1.png", Frame(0,0,128,128), 10, 'horizontal')
